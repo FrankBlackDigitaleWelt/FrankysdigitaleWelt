757ea99a60a10946a77593ee15bfef2f5c0e45e2
Frank <122209476+FrankBlackDigitaleWelt@users.noreply.github.com>
2023-02-07 17:10:25 +0100

    Add files via upload

---
+++ login.js
@@ -0,0 +1,10 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username == "chat" && password == "programming") {
+      window.open("chat.html", "Chat", "width=400,height=400");
+    } else {
+      document.getElementById("message").innerHTML = "Falscher Benutzername oder Passwort";
+    }
+  }
+  
